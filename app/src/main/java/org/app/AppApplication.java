0d3c5f8e6548da07baa4cab607506acd3258f983
mkopczewski <kopczewski.michal101@gmail.com>
2024-12-11 16:04:14 +0100

    Działa konteneryzacja z budowaniem 2etapowym i siecią między kontenerami

--- app/src/main/java/org/app/AppApplication.java
+++ app/src/main/java/org/app/AppApplication.java
@@ -11,7 +11,9 @@ public class AppApplication {
     Dotenv dotenv = Dotenv.configure().load();
     System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
     System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
-        SpringApplication.run(AppApplication.class, args);
+        System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
+    SpringApplication.run(AppApplication.class, args);
+
     }
 
 }
