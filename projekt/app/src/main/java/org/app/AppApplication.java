01bcfe200fca478ad48db1dfe3ea7c3f42db5866
mkopczewski <kopczewski.michal101@gmail.com>
2025-02-04 03:08:51 +0100

    Problemy ze wstniem aplikacji

--- projekt/app/src/main/java/org/app/AppApplication.java
+++ projekt/app/src/main/java/org/app/AppApplication.java
@@ -8,12 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
     public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure().load();
-    System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
-    System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
-    System.setProperty("MYSQL_URL", dotenv.get("MYSQL_URL"));
     SpringApplication.run(AppApplication.class, args);
 
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("MYSQL_USER: " + System.getenv("MYSQL_USER"));
+        System.out.println("MYSQL_PASSWORD: " + System.getenv("MYSQL_PASSWORD"));
+        System.out.println("MYSQL_URL: " + System.getenv("MYSQL_URL"));
     }
 
 }
--- projekt/kubernetes/start.bat
+++ projekt/kubernetes/start.bat
@@ -1,5 +1,7 @@
 @echo off
-kubectl apply -f configmap.yaml
+kubectl apply -f app-configmap.yaml
 kubectl apply -f db-pvc.yaml
 kubectl apply -f db-deployment.yaml
 kubectl apply -f db-service.yaml
+kubectl apply -f app-deployment.yaml
+kubectl apply -f app-service.yaml
